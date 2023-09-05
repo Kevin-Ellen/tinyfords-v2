9449460424848c46615b43b3ca7093b3b8991e92
Kevin Ellen <45627930+Kevin-Ellen@users.noreply.github.com>
2023-09-05 19:07:25 +0100

    Comments

--- src/index.js
+++ src/index.js
@@ -1,41 +1,54 @@
-// src > index.js - entry point
+/**
+ * index.js
+ * 
+ * This module serves as the main entry point for handling all incoming requests.
+ * It routes requests based on their URL path, delegating to specific handlers for processing.
+ * The module can handle both static assets like images and fonts, as well as dynamic routes 
+ * for rendering templates. If a request doesn't match any predefined route, an error response is returned.
+ */
 
+// Importing necessary modules
 import indexAdmin from './admin/indexAdmin';
-
 import handlerStatic from './lib/handlers/static';
 import handlerTemplate from './lib/handlers/template';
 
+// Adding an event listener to listen for 'fetch' events. This event is triggered for every HTTP request coming to the service.
 addEventListener('fetch', event => {
   const url = new URL(event.request.url);
   
+  // Checking if the request URL starts with '/admin' and routing it to the admin handler.
   if (url.pathname.startsWith('/admin')) {
     event.respondWith(indexAdmin(event.request));
-  }else{
+  } else {
+    // For other paths, it uses the handleRequest function.
     event.respondWith(handleRequest(event.request));
   }
-})
+});
 
-
+// The main function for routing requests based on their path and method.
 const handleRequest = async (request) => {
-
   const url = new URL(request.url);
-  url.params = new URLSearchParams(url.search);
+  url.params = new URLSearchParams(url.search); // Extracting query parameters
 
+  // Check if the path starts with '/images/' or '/fonts/' to handle static assets.
   if((url.pathname.startsWith('/images/')) || (url.pathname.startsWith('/fonts/'))){
     return handlerStatic(url) || handlerError();
   }
 
+  // Handling dynamic routes using template handler. You can add more paths as your application grows.
   switch (url.pathname){
     case '/robots.txt': 
       return handlerStatic(url) || handlerError();
 
     case '/':
       return handlerTemplate(url) || handlerError();
-
   }
+
+  // If none of the above paths match, return an error response.
   return handlerError();
 }
 
+// A generic error handler for unmatched routes or other errors.
 const handlerError = () => {
   return new Response('not done yet', {status:404});
 }
--- src/lib/utils/searchTools.js
+++ src/lib/utils/searchTools.js
@@ -1,33 +1,47 @@
-// src > lib > utils > searchTools.js - All search tools
+/**
+ * searchTools.js
+ * 
+ * This module contains utility functions to assist with searching and shaping
+ * car data for querying. The tools allow for specific filtering of car data
+ * based on various criteria, such as photo availability, case details, category,
+ * and more.
+ */
 
 import { getCaseById, getCategoryById } from "./dataCars";
 
+/**
+ * Shapes and prepares the search form data for querying.
+ * 
+ * @param {Array} dataCarsAll - All car data.
+ * @param {Object} data - The search criteria.
+ * @returns {Object} - The shaped search criteria.
+ */
 export const toolShapeSearchFormData = (dataCarsAll, data) => {
   const shapedData = {};
 
-  // Process hasPhoto
+  // Process the hasPhoto property
   if ('hasPhoto' in data) {
     shapedData.hasPhoto = data.hasPhoto === "true";
   }
 
-  // Process hasCase and caseDetails
+  // Handle the hasCase property and related case details
   if( 'hasCase' in data ){
-
     if (data.caseType) {
       const caseDetails = getCaseById(dataCarsAll, data.caseType);
       if (caseDetails) {
         shapedData.caseDetails = caseDetails;
         shapedData.caseDetails.status = shapedData.hasCase;
       }
-    }else{
+    } else {
       shapedData.caseDetails = {};
     }
     shapedData.caseDetails.status = data.hasCase === "true" ? true : data.hasCase === "false" ? false : null;
 
+    // Clean up temporary properties
     ['hasCase', 'caseType'].forEach(prop => delete data[prop]);
   }
 
-  // Process categoryDetails
+  // Process the category details
   if (data.category) {
     const categoryDetails = getCategoryById(dataCarsAll, data.category);
     if (categoryDetails) {
@@ -36,15 +50,16 @@ export const toolShapeSearchFormData = (dataCarsAll, data) => {
     ['category'].forEach(prop => delete data[prop]);
   }
 
+  // Convert id to an integer if present
   if(data.id){
     data.id = parseInt(data.id, 10);
   }
 
-  // Transfer other properties without change
+  // Copy other properties without any transformation
   for (const key in data) {
     shapedData[key] = data[key];
 
-    // Remove empty or null properties
+    // Remove any properties that are empty or null
     if (shapedData[key] === "" || shapedData[key] === null) {
       delete shapedData[key];
     }
@@ -53,18 +68,25 @@ export const toolShapeSearchFormData = (dataCarsAll, data) => {
   return shapedData;
 }
 
+/**
+ * Filters a car based on provided criteria.
+ * 
+ * @param {Object} car - The car data.
+ * @param {Object} criteria - The search criteria.
+ * @returns {boolean} - Whether the car matches the criteria or not.
+ */
 export const toolCarSearch = (car, criteria) => {
   for (const key in criteria) {
     if (typeof criteria[key] === 'object' && criteria[key] !== null) {
-      // If the criteria is an object (like caseDetails), we check its sub-properties
+      // If the criteria is an object (like caseDetails), we iterate through its sub-properties
       for (const subKey in criteria[key]) {
         if (car[key][subKey] !== criteria[key][subKey]) {
-          return false; // Sub-property doesn't match
+          return false; // If any sub-property doesn't match, the car doesn't meet the criteria
         }
       }
     } else if (car[key] !== criteria[key]) {
-      return false; // Direct property doesn't match
+      return false; // If any direct property doesn't match, the car doesn't meet the criteria
     }
   }
-  return true; // All criteria matched
+  return true; // The car meets all provided criteria
 }
