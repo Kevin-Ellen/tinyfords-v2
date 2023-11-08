1155acf8bacfb3433112eb09ede983b5e2f78baf
Kevin Ellen <45627930+Kevin-Ellen@users.noreply.github.com>
2023-11-08 15:45:40 +0000

    gitignore changes

--- .wrangler/tmp/bundle-NSBAD8/middleware-insertion-facade.js
+++
@@ -1,3 +0,0 @@
-import { __facade_registerInternal__ } from "/usr/local/lib/node_modules/wrangler/templates/middleware/loader-sw.ts";
-import * as __MIDDLEWARE_0__ from "/usr/local/lib/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts";
-__facade_registerInternal__([__MIDDLEWARE_0__.default])
--- .wrangler/tmp/dev-MFLdbg/964b05abc6ae215eb1b1ec66020f594020a14e77-template-spa.js
+++
@@ -1,7 +0,0 @@
-const handlerTemplate = (data = appData.pages.current) => {
-  switch (data.template){
-    case 'home': renderHome();break;
-    case 'collection': renderCollection();break;
-    case 'content': renderContent();break;
-  }
-}
